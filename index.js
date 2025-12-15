d6871202b64883ef7cb6d306d6597d38e3c54765
Nishant Sharma <nishant.sh230408@gmail.com>
2025-12-15 13:39:53 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenVal = '';
+
+for(item of buttons){
+    item.addEventListener('click', (e) =>{
+        buttonText = e.target.innerText;
+        console.log(buttonText);
+        if(buttonText == 'x'){
+            buttonText = '*';
+            screenVal += buttonText;
+            screen.value = screenVal;
+        }
+        else if(buttonText == 'C'){
+            screenVal = '';
+            screen.value = screenVal;
+
+        }
+        else if(buttonText == '='){
+            screen.value = eval(screenVal)
+        }
+        else if(buttonText == '÷'){
+            buttonText = '/';
+            screenVal += buttonText;
+            screen.value = screenVal;
+        }
+        else{
+            screenVal += buttonText;
+            screen.value = screenVal;
+        }
+    })
+}
